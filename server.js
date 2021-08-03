02d6fcb1e24478a46fd8e0ee96bfe8c322d847aa
varanshu-garg <varanshugarg@gmail.com>
2021-08-03 11:58:58 +0530

    better

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.urlencoded({ extended : true}))
 app.post('/refresh',(req, res) =>{
     const refreshToken= req.body.refreshToken
     const spotifyApi = new SpotifyWebApi({
-        redirectUri: process.env.REDIRECT_URI,
+        redirectUri:"https://focused-agnesi-5420f0.netlify.app",
         clientId: process.env.CLIENT_ID,
         clientSecret: process.env.CLIENT_SECRET,
         refreshToken,
@@ -36,7 +36,7 @@ app.post('/login',(req, res) =>{
 
     const code= req.body.code
     const spotifyApi = new SpotifyWebApi({
-        redirectUri: process.env.REDIRECT_URI,
+        redirectUri: "https://focused-agnesi-5420f0.netlify.app",
         clientId: process.env.CLIENT_ID,
         clientSecret: process.env.CLIENT_SECRET,
     })
